30df02b7e7bac3f624d7ba354a8e7602b2f92904
Guilherme <161765747+guilhermeantoniosf@users.noreply.github.com>
2024-05-18 19:40:49 -0300

    criação dos exércicios 9 e 8, e desafio da aula 11

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocida do seu é ${vel}km/h`)
+if (vel > 60){ // Condição Simples
+    console.log(`Multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança, se quiser morrer`)
