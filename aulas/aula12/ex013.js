659d2c173e557985408190abc42ed273b0143016
Guilherme <161765747+guilhermeantoniosf@users.noreply.github.com>
2024-05-19 21:55:06 -0300

    mundanças no ex013

--- aulas/aula12/ex013.js
+++ aulas/aula12/ex013.js
@@ -1,5 +1,30 @@
 var agora = new Date()
-var diasem = agora.getDay()
-
+var diasemana = agora.getDay()
+diasemana = 1
 /*
-0 = Domingo*/
+0 = Domingo
+1 = Segunda
+2 = Terça
+3 = Segunda
+4 = Segunda
+5 = Segunda
+*/
+console.log(diasemana)
+switch(diasemana) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    
+    default: 
+    console.log('Dia inválido')
+    break
+}
