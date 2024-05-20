715bc7c75b0dd2aca75a428c767d01bb88b87346
Guilherme <161765747+guilhermeantoniosf@users.noreply.github.com>
2024-05-20 10:02:58 -0300

    criação dos desafios 1 e 2

--- aulas/aula12/ex013.js
+++ aulas/aula12/ex013.js
@@ -1,30 +1,30 @@
 var agora = new Date()
 var diasemana = agora.getDay()
-diasemana = 1
+diasemana = 3
 /*
 0 = Domingo
 1 = Segunda
 2 = Terça
-3 = Segunda
+3 = Quarta
 4 = Segunda
 5 = Segunda
 */
 console.log(diasemana)
 switch(diasemana) {
     case 0:
-        console.log('Domingo')
+        console.log('Domingo');
         break
     case 1:
-        console.log('Segunda')
+        console.log('Segunda');
         break
     case 2:
-        console.log('Terça')
+        console.log('Terça');
         break
     case 3:
-        console.log('Quarta')
+        console.log('Quarta');
         break
     
     default: 
-    console.log('Dia inválido')
+    console.log('Dia inválido');
     break
 }
